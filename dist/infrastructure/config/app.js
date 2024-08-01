338ba88ea26ba96bcd8bbbd83c27b420265b4e0b
Abhinand K <abhinand8413@gmail.com>
2024-08-01 10:21:40 +0530

    Fix: base url issue

--- dist/infrastructure/config/app.js
+++ dist/infrastructure/config/app.js
@@ -28,8 +28,7 @@ const createServer = () => {
         app.use(express_1.default.urlencoded({ extended: true }));
         app.use((0, cookie_parser_1.default)());
         app.use((0, cors_1.default)({
-            // origin: ["http://localhost:5173"],
-            origin: ["https://mockq.vercel.app"],
+            origin: process.env.BASE_URL,
             methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
             credentials: true,
         }));
--- src/infrastructure/config/app.ts
+++ src/infrastructure/config/app.ts
@@ -28,9 +28,8 @@ const createServer = () => {
 
     app.use(cookieParser());
     app.use(
-      cors({
-        // origin: ["http://localhost:5173"],
-        origin: ["https://mockq.vercel.app"], 
+      cors({ 
+        origin: process.env.BASE_URL, 
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         credentials: true,
       })
